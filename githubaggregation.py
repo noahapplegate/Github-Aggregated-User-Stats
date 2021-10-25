697a6d2864137552966d9ffdb22ea0543fd5d7dd
Noah Applegate <noahapplegate1@gmail.com>
2021-10-24 21:31:21 -0700

    Add home page

--- githubaggregation.py
+++ githubaggregation.py
@@ -65,6 +65,20 @@ def aggregate_github_stats(username, show_forked):
     return aggregate_data_response
 
 
+@app.route('/')
+def home():
+    return '''<h2>Github user's aggregate stats</h2>
+              <h4 style="margin-left:5%">Returns JSON containing a github user's total number of repos, total number of 
+              stargazers, total number of forks, average repo size in KB, and a list of languages used.</h4>
+              <h3>How to use:</h3>
+              <div style="margin-left:5%">
+                <p>To use this API visit https://radiant-garden-83945.herokuapp.com/{username}</p>
+                <p>To filter out data from repositories that have been forked, 
+                set the query string parameter 'forked=0'</p>
+                <p>Example: https://radiant-garden-83945.herokuapp.com/fabpot?forked=0</p>
+              </div>'''
+
+
 @app.route('/<string:username>')
 def github_user_stats(username):
     # Validate the username
